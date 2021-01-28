af525dcfc8033d9a8d7bfe99e02cc3f6a838836a
31vector31 <lutaiyura@gmail.com>
2021-01-28 20:04:02 +0200

    problema

--- src/App.js
+++ src/App.js
@@ -2,47 +2,57 @@ import logo from './logo.svg';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import React, { useState} from 'react'
 
-
+let width = Array(5).fill(<th><input type="text" size="1"/></th>);
 
 function App() {
-  return (
+
+    return (
     <div className="App">
 
       <form name="test">
 
-        <input type="text" size="10"/>
+        <input id="Width" type="text" size="10"/>
         <input type="text" size="10"/>
 
 
-        <input type="submit" value="Го"/>
+        <input type="submit" onClick={
+            () => {
+                /*alert(document.getElementById('Width').value);*/
+                width = Array(document.getElementById('Width').value).fill(<th><input type="text" size="1"/></th>);
+            }
+        } value="Го"/>
 
       </form>
 
-       <form>
-  <TextField id="Width" label="Width" defaultValue={5}/>
-  <TextField id="Height" label="Height" defaultValue={5}/>
-  
-  <Button variant="contained">Go</Button>
 
-</form>
+       {/*<form>
+  <TextField ref="Width" label="Width" defaultValue={5} />
+  <TextField id="Height" label="Height" defaultValue={5} />
+  
+  <Button variant="contained" onClick={() => {
+      alert()
+
+  }} >Go</Button>
+
+</form>*/}
+
+
 
 
 
 <table>
-   
    <tr>
-    <th><input type="text" size="1"/></th>
-    <th><input type="text" size="1"/></th>
-    <th><input type="text" size="1"/></th>
-    <th><input type="text" size="1"/></th>
+       {width.map(animal => (
+           <th>{animal}</th>
+       ))}
    </tr>
    
     <tr>
-        <th><input type="text" size="1"/></th>
-        <th><input type="text" size="1"/></th>
-        <th><input type="text" size="1"/></th>
-        <th><input type="text" size="1"/></th>
+        {width.map(animal => (
+            <th>{animal}</th>
+        ))}
    </tr>
    
   </table>
@@ -50,5 +60,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
